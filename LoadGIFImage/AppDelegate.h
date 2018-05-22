3bce040b666024c894411133834b8cd080f6225e
DuWenLiang <739285514@qq.com>
2018-05-22 14:29:43 +0800

    Initial Commit

---
+++ LoadGIFImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadGIFImage
+//
+//  Created by 杜文亮 on 2018/5/22.
+//  Copyright © 2018年 心动文学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
